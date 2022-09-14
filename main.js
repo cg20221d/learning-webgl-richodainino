e708c705de435c0430f3ab0a8571bed56e9bb2d6
Richo <daininoricho@yahoo.com>
2022-09-14 14:51:18 +0700

    feat: gambar 1

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ function main() {
 	const gl = canvas.getContext("webgl")
 	
 	// Vertex Shader
-	var vertexShaderCode = 
-	"void main() {" +
-	"}"
+	var vertexShaderCode = `
+	void main() {
+		float x = 0.0;
+		float y = 0.0;
+		gl_PointSize = 10.0;
+		gl_Position = vec4(x, y, 0.0, 1.0);
+	}
+	`
 	const vertexShaderObject = gl.createShader(gl.VERTEX_SHADER)
 	gl.shaderSource(vertexShaderObject, vertexShaderCode)
 	gl.compileShader(vertexShaderObject) // sampai sini jadi .o
 	
 	// Fragment Shader
 	var fragmentShaderCode = `
+	precision mediump float;
 	void main() {
-		
-	}`
+		float r = 0.0;
+		float g = 0.0;
+		float b = 1.0;
+		gl_FragColor = vec4(r, g, b, 1.0);
+	}
+	`
 	const fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER)
 	gl.shaderSource(fragmentShaderObject, fragmentShaderCode)
 	gl.compileShader(fragmentShaderObject) // sampai sini jadi .o
@@ -29,4 +39,6 @@ function main() {
 	gl.clearColor(1.0, 1.0, 0, 1.0)
 
 	gl.clear(gl.COLOR_BUFFER_BIT)
+
+	gl.drawArrays(gl.POINTS, 0, 1)
 }
