639f6b344af4c408f86c657855204e705b0a5850
Richo <daininoricho@yahoo.com>
2022-09-14 15:11:11 +0700

    feat: menambahkan gambar 3 titik

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ function main() {
 	const canvas = document.getElementById("canvas")
 	const gl = canvas.getContext("webgl")
 	
+	var vertices = [
+		0.5, 0.5,	// A: kanan atas
+		0.0, 0.0,	// B: bawah tengah
+		-0.5, -0.5	// C: kiri atas
+	]
+
+	var buffer = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+
 	// Vertex Shader
 	var vertexShaderCode = `
+	attribute vec2 aPosition;
 	void main() {
-		float x = 0.0;
-		float y = 0.0;
+		float x = aPosition.x;
+		float y = aPosition.y;
 		gl_PointSize = 10.0;
 		gl_Position = vec4(x, y, 0.0, 1.0);
 	}
@@ -36,9 +47,14 @@ function main() {
 	gl.linkProgram(shaderProgram)
 	gl.useProgram(shaderProgram)
 
-	gl.clearColor(1.0, 1.0, 0, 1.0)
+	// Mengajari GPU cara mengoleksi nilai posisi dari ARRAY_BUFFER untuk setiap verteks yang diproses
+	var aPosition = gl.getAttribLocation(shaderProgram, "aPosition")
+	gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0)
+	gl.enableVertexAttribArray(aPosition)
+
+	gl.clearColor(1.0, 0.65, 0, 1.0)
 
 	gl.clear(gl.COLOR_BUFFER_BIT)
 
-	gl.drawArrays(gl.POINTS, 0, 1)
+	gl.drawArrays(gl.POINTS, 0, 3)
 }
