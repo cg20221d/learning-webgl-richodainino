bd88c999ef41ae5fa4c3a63050f261e2969093f1
Richo <daininoricho@yahoo.com>
2022-09-21 15:07:16 +0700

    feat: make it movable

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ function main() {
 	/** @type {HTMLCanvasElement} */
 	const canvas = document.getElementById("canvas")
 	const gl = canvas.getContext("webgl")
-	
+
 	var vertices = [
-		0.5, 0.0, 0.0, 1.0, 1.0,		// A: kanan atas		CYAN
-		0.0, -0.5, 1.0, 0.0, 1.0,		// B: bawah tengah		MAGENTA
-		-0.5, 0.0, 1.0, 1.0, 0.0,		// C: kiri atas			YELLOW
-		0.0, 0.5, 1.0, 1.0, 1.0			// D: atas tengah		KEY
+		0.5, 0.0,   0.0, 1.0, 1.0,	// A: kanan atas		CYAN
+		0.0, -0.5,  1.0, 0.0, 1.0,	// B: bawah tengah		MAGENTA
+		-0.5, 0.0,  1.0, 1.0, 0.0,	// C: kiri atas			YELLOW
+		0.0, 0.5,   1.0, 1.0, 1.0,	// D: atas tengah		KEY
 	]
 
 	var buffer = gl.createBuffer()
@@ -19,10 +19,11 @@ function main() {
 	attribute vec2 aPosition;
 	attribute vec3 aColor;
 	uniform float uTheta;
+	uniform vec2 uTranslation;
 	varying vec3 vColor;
 	void main() {
-		float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
-		float y = cos(uTheta) * aPosition.x + sin(uTheta) * aPosition.y;
+		float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y + uTranslation.x;
+		float y = cos(uTheta) * aPosition.x + sin(uTheta) * aPosition.y + uTranslation.y;
 		gl_Position = vec4(x, y, 0.0, 1.0);
 		vColor = aColor;
 	}
@@ -52,9 +53,11 @@ function main() {
 	// Variabel lokal
 	var theta = 0.0
 	var freeze = false
+	var translation = [0.0, 0.0]
 
 	// Variabel pointer ke GLSL
-	var uThete = gl.getUniformLocation(shaderProgram, "uTheta")
+	var uTheta = gl.getUniformLocation(shaderProgram, "uTheta")
+	var uTranslation = gl.getUniformLocation(shaderProgram, "uTranslation")
 
 	// Mengajari GPU cara mengoleksi nilai posisi dari ARRAY_BUFFER untuk setiap verteks yang diproses
 	var aPosition = gl.getAttribLocation(shaderProgram, "aPosition")
@@ -85,14 +88,75 @@ function main() {
 	document.addEventListener("keydown", onKeyDown)
 	document.addEventListener("keyup", onKeyUp)
 
+	var controller = {
+		w: false,
+		a: false,
+		s: false,
+		d: false
+	}
+
+	window.onkeydown = event => {
+		if (event.key == 'w')
+		{
+			controller.w = true
+		} 
+		if (event.key == 'a')
+		{
+			controller.a = true
+		}
+		if (event.key == 's')
+		{
+			controller.s = true
+		}
+		if (event.key == 'd')
+		{
+			controller.d = true
+		}
+	}
+
+	window.onkeyup = event => {
+		if (event.key == 'w')
+		{
+			controller.w = false
+		} 
+		if (event.key == 'a')
+		{
+			controller.a = false
+		}
+		if (event.key == 's')
+		{
+			controller.s = false
+		}
+		if (event.key == 'd')
+		{
+			controller.d = false
+		}
+	}
 
 	function render() {
 		gl.clearColor(0.0, 0.0, 0.0, 0.03)
 		gl.clear(gl.COLOR_BUFFER_BIT)
+
 		if (!freeze) {
 			theta += 0.1
-			gl.uniform1f(uThete, theta)
 		}
+
+		if (controller.w) {
+			translation[1] += 0.01
+		}
+		if (controller.a) {
+			translation[0] -= 0.01
+		}
+		if (controller.s) {
+			translation[1] -= 0.01
+		}
+		if (controller.d) {
+			translation[0] += 0.01
+		}
+
+		gl.uniform1f(uTheta, theta)
+		gl.uniform2fv(uTranslation, translation)
+
 		gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
 		requestAnimationFrame(render)
 	}
